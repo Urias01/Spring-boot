1c70b27f55c45a8d569b0f5a4360590a877a6b7b
Urias01 <uriasluis64@gmail.com>
2022-05-24 18:26:20 -0300

    h2 database, test profile, JPA

--- src/main/java/com/couserjava/course/entities/User.java
+++ src/main/java/com/couserjava/course/entities/User.java
@@ -1,11 +1,16 @@
 package com.couserjava.course.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
+@Table(name="users")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
